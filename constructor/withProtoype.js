6dd85565772ff4b23195bb44f207bd176eea9870
sdkcay <sidikaakoyun@yahoo.com>
2019-05-09 22:07:52 +0300

    constructor pattern

---
+++ constructor/withProtoype.js
@@ -0,0 +1,24 @@
+/**
+ * JSDesignPatterns
+ * withProtoype.js
+ * Created by Sıdıka ÇAY on 2019-05-08
+ */
+
+"use strict";
+
+/* use to prepare object for use and accept parameters to set values to variables*/
+const  Car = function (model, color) {
+    this.model = model;
+    this.color = color;
+
+    //to define a function using prototype, create a single instance and shared between all Car obj
+    Car.prototype.toString = function () {
+        return "This " + this.model + " is " + this.color;
+    }
+};
+
+const peugeout = new Car("Peugeout", "black");
+const bmw = new Car("BMW", "red");
+
+console.log(peugeout.toString());
+console.log(bmw.toString());
